18f4e63b2efc241b2783882d65199a4b5ba1085f
jdn <spiritvt1211@gmail.com>
2017-08-24 23:08:30 +0700

    homework

---
+++ homework9/godTower/Assets/script/level3script.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class level3script : MonoBehaviour {
+    public Text levelText;
+    public Text hinText;
+    public Image hint2Image;
+    public string levelContent = "LEVELS";
+    public string levelNumber;
+    public InputField inputField;
+    public Button submitButton;
+    public string levelAnswer;
+    public int count = 0;
+    string answer;
+    // Use this for initialization
+    void Start () {
+        levelText.text = levelContent;
+        StartCoroutine(ChangeLevelTextRoutine());
+        hint2Image.gameObject.SetActive(false);
+    }
+    public void GetInput()
+    {
+        answer = inputField.text;
+        CheckAnswer(answer.ToLower());
+    }
+    public void GetHint()
+    {
+        count++;
+        if(count % 2 == 1)
+        {
+            hint2Image.gameObject.SetActive(true);
+        }
+        else hint2Image.gameObject.SetActive(false);
+
+
+    }
+
+    public void CheckAnswer(string answer)
+    {
+        if (answer == levelAnswer)
+        {
+            hinText.text = "Yayyyy";
+            //TODO: Change scene
+        }
+        else
+        {
+            hinText.text = "WRONGGGG!!!!!";
+            hinText.color = Color.red;
+            inputField.text = "";
+            inputField.ActivateInputField();
+        }
+    }
+    IEnumerator ChangeLevelTextRoutine()
+    {
+        while (true)
+        {
+            levelText.text = levelContent;
+            //Wait for 2 seconds
+            yield return new WaitForSeconds(2f);
+            levelText.text = levelNumber;
+            yield return new WaitForSeconds(2f);
+        }
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ homework9/godTower/Assets/script/startScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class startScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    public void LetGo()
+    {
+        SceneManager.LoadScene(1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
